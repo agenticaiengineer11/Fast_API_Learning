e4ac37faa1d52c532e42b3b0b1480ea20e7d33dd
ranjhanoman75-ai <ranjhanoman75@gmail.com>
2026-07-12 11:30:30 +0500

    Libray api updated

--- library_api.py
+++ library_api.py
@@ -1,13 +1,22 @@
 print("==========Library_API=========")
 from fastapi import FastAPI
 books = [
-    {"id":2,"chapters":2,"chapter_id": 134},
-    {"id":3,"chapters":4,"chapter_id": 123}
+    {"id":2,"title":"python","chapter_id": 134},
+    {"id":3,"title":"c++","chapter_id": 123}
 ]
 app  = FastAPI()
 @app.get("/books")
 def get():
     return books
+
+@app.get("/books/search")
+def search_book(title:str,book_id:int):
+    for book in books:
+        if book["title"] == title and book["id"] == book_id:
+            return book
+    return {
+        "message": "Not found"
+    }
 @app.get("/books/{book_id}")
 def get_book(book_id:int):
     for book in books:
@@ -16,6 +25,7 @@ def get_book(book_id:int):
     return{
         "message": "Book not found"
     }
+
 @app.get("/books/{book_id}/chapters/{chapter_id}")
 def get_book_chapters(book_id:int,chapter_id:int):
     for book in books:
