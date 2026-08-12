11661fed5095cbca0406432f4e0098e62fc4c681
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-12 10:40:51 +0500

    pydantic dependency is updated

--- pydanticdependency.py
+++ pydanticdependency.py
@@ -1,38 +1,43 @@
-print("======================Pydantic&Dependency=====================")
-
-from fastapi import FastAPI,Depends,Query
+from fastapi import FastAPI, Depends, Query
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
-class common_filters(BaseModel):
-    search: str | None =None
+
+class ProductFilters(BaseModel):
+    category: Optional[str] = None
     page: int = 1
     limit: int = 10
 
-def get_filters(
-        search: str | None = None,
-        page: int = Query(1,ge=1),
-        limit :int = Query(10,ge=1,le=100)
+
+def get_product_filters(
+    category: Optional[str] = None,
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, ge=1, le=100)
 ):
-    return common_filters(
-        search=search,
+    return ProductFilters(
+        category=category,
         page=page,
         limit=limit
     )
+
+
 @app.get("/products")
 def get_products(
-    filters:common_filters = Depends(get_filters)
+    filters: ProductFilters = Depends(get_product_filters)
 ):
-    return{
-        "products": ["Laptop","charger"],
-        "filter": filters
+    return {
+        "products": ["Mobile", "iPad"],
+        "filters": filters
     }
-@app.get("/orders")
-def get_orders(
-    filters:common_filters = Depends(get_filters)
+
+
+@app.get("/featured-products")
+def get_featured_products(
+    filters: ProductFilters = Depends(get_product_filters)
 ):
-    return{
-        "orders": ["order1","order2"],
+    return {
+        "featured_products": ["iPhone", "iPad2"],
         "filters": filters
     }
