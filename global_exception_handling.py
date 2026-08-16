090f2d183f675f32fb5b65413b1367bc3888fd46
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-16 10:08:08 +0500

    Global exception handling file implemented

---
+++ global_exception_handling.py
@@ -0,0 +1,36 @@
+print("======================Global exception handling===================")
+
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(
+    request: Request,
+    exc: HTTPException
+):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "success": False,
+            "error": exc.detail,
+            "status_code": exc.status_code
+        }
+    )
+
+
+@app.get("/students/{student_id}")
+def get_student(student_id: int):
+
+    if student_id != 1:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+    return {
+        "id": 1,
+        "name": "Noman"
+    }
